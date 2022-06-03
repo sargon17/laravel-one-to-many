1474f942c224f37d1ebbe9da409a787f71436f80
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-06-03 15:44:17 +0200

    Update Post.php

--- app/Post.php
+++ app/Post.php
@@ -7,7 +7,12 @@ use Illuminate\Support\Str;
 
 class Post extends Model
 {
-    protected $fillable = ["title", "content", "slug"];
+    protected $fillable = ["title", "content", "slug", "category_id"];
+
+    public function category()
+    {
+        return $this->belongsTo("App\Category");
+    }
 
     public static function createSlug($title)
     {
