7a4add804fcd799e7ce5f2dbe732c293d51edffd
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-06-03 16:44:44 +0200

    Update edit.blade.php

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -30,12 +30,21 @@
                             </div>
                         @endif
                         <label for="title">Titolo</label>
-                        @if ($errors->has('title'))
-                        <input type="text" class="form-control is-invalid" id="title" name="title" placeholder="Titolo" value=" {{ old('title', $post->title ) }} ">
+                        <input type="text" class="form-control  @error('title') is-invalid @enderror" id="title" name="title" placeholder="Titolo" value=" {{ $post->title }} ">
                         <div class="invalid-feedback">{{ $errors->first('title') }}</div>
-                        @else
-                        <input type="text" class="form-control" id="title" name="title" placeholder="Titolo" value=" {{ old('title', $post->title) }} ">
-                        @endif
+                    </div>
+                    <div class="form-group">
+                        <select class="form-select  @error('category_id') is-invalid @enderror is-invalid" name="category_id">
+                          <option selected>Categories</option>
+                          @foreach ($categories as $category)
+                          <option value="{{ $category->id }}"
+                                {{ old('category_id', $post->category_id) == $category->id ? 'selected' : '' }}
+                                >{{ $category->name }}</option>
+                          @endforeach
+                        </select>
+                    @error('category_id')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                     </div>
                     <div class="form-group">
                         <label for="slug">Slug</label>
@@ -43,12 +52,8 @@
                     </div>
                     <div class="form-group">
                         <label for="content">Contenuto</label>
-                        @if ($errors->has('content'))
-                        <textarea class="form-control is-invalid" id="content" name="content" rows="3">{{ old('content', $post->content) }}</textarea>
+                        <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content" rows="3">{{ $post->content }}</textarea>
                         <div class="invalid-feedback">{{ $errors->first('content') }}</div>
-                        @else
-                        <textarea class="form-control" id="content" name="content" rows="3">{{ old('content', $post->content) }}</textarea>
-                        @endif
                     </div>
                     <button type="submit" class="btn btn-primary">Update</button>
                 </form>
